01396c14391260e66dbad11fc1006d3511fdfc46
Danniel Cristie <dany14012@gmail.com>
2022-06-18 22:56:51 -0300

    chore: controle de candidatos.
    - tela de controle de candidatos que se candidaram a vaga;
    - ajustes de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
     root to: "vacancies#all", as: "unauthenticated_root"
   end
 
-  resources :applicants
+  resources :applicants, only: :create
+  resources :vacancies do
+    resources :applicants, only: %i[index]
+  end
   resources :vacancies
   resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
