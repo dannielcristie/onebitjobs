e2c3358deb8f34ddb7cdd0c0391097d4d24d2f70
Danniel Cristie <dany14012@gmail.com>
2022-06-30 10:46:03 -0300

    chore: config controller
    - add devise parameter(strongs params) para update do Model company.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email name description password password_confirmation])
   end
 end
